7363ac8df15a5f68a882ad1859b0b57eb0292006
Zhang Zeying <zhangzeyingvv@gmail.com>
2021-05-18 17:31:26 +0800

    Add files via upload

---
+++ Kernel/init.m
@@ -0,0 +1,13 @@
+(* ::Package:: *)
+
+(* Mathematica Init File *)
+Get["MagneticTB`Usage`"]
+Get["MagneticTB`magtb`"]
+Get["MagneticTB`plot`"]
+Get["MagneticTB`IO`"]
+Get["MagneticTB`WilsonLoop`"]
+Get["MagneticTB`Utilities`"]
+(*{pointOpLabel,pointOpXyz,pointOpMatrix,pointOpHexLabel,pointOpHexXyz,pointOpHexMatrix,
+braLatt,gray,typeI,typeIII,typeIV}=Import[$UserBaseDirectory<>"/Applications/magtb/"<>"opdata.m"];*)
+(*Get["tb2`WilsonLoop`"]*)
+
